79f0f174af3776f34d07877f49b33ef7ed3d15ce
KL-Huang <craint1001@gmail.com>
2025-06-18 19:38:28 +0800

    commit

--- scripts/utils/data_processing_gold_feature.py
+++ scripts/utils/data_processing_gold_feature.py
@@ -1,5 +1,4 @@
 import os
-import glob
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
@@ -14,45 +13,6 @@ import argparse
 from pyspark.sql.functions import col
 from pyspark.sql.types import StringType, IntegerType, FloatType, DateType
 
-
-
-def read_silver_table(table, silver_directory, spark):
-    """
-    Helper function to read all partitions of a silver table
-    """
-    folder_path = os.path.join(silver_directory, table)
-    files_list = [os.path.join(folder_path, os.path.basename(f)) for f in glob.glob(os.path.join(folder_path, '*'))]
-    df = spark.read.option("header", "true").parquet(*files_list)
-    return df
-
-# Define paths
-silver_directory = "datamart/silver"
-gold_directory = "datamart/gold"
-os.makedirs(gold_directory, exist_ok=True)
-print(f"Gold root directory: {gold_directory}")
-
-gold_feature_directory = "datamart/gold/feature_store/"
-if not os.path.exists(gold_feature_directory):
-    os.makedirs(gold_feature_directory)
-
-# cust_df = read_silver_table('orders', silver_directory, spark)
-# geo_df = read_silver_table('orders', silver_directory, spark)
-# items_df = read_silver_table('orders', silver_directory, spark)\
-# # to be changed
-# logistic_df = read_silver_table('orders', silver_directory, spark)
-# prod_df = read_silver_table('orders', silver_directory, spark)
-# sellers_df = read_silver_table('orders', silver_directory, spark)
-orders_df = read_silver_table('orders', silver_directory, spark)
-
-parser = argparse.ArgumentParser(description='Process snapshot date from Airflow')
-parser.add_argument('--startdate', type=str, required=True, help='Snapshot date in YYYY-MM-DD format')
-args = parser.parse_args()
-snapshot_date_str = args.startdate
-
-
-############################
-# Pipeline
-############################
 
 def process_feature_gold_table(snapshot_date_str, silver_directory, gold_directory, orders_df, spark):
 # def process_feature_gold_table(snapshot_date_str, silver_directory, gold_directory, 
@@ -75,7 +35,9 @@ def process_feature_gold_table(snapshot_date_str, silver_directory, gold_directo
 
     # Save gold table - output only for the given snapshot date
     partition_name = f"gold_feature_store_{snapshot_date_str.replace('-','_')}.parquet"
-    filepath = os.path.join(gold_directory, partition_name)
+    filepath = os.path.join(gold_directory + "/feature_store/" + partition_name)
     df.write.mode("overwrite").parquet(filepath)
     print('saved to:', filepath)
     print('Feature gold table processing completed for snapshot date:', snapshot_date_str)
+
+    return df
