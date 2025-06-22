9922261d96f75c426cbbc9b3390212770d98d286
KL-Huang <craint1001@gmail.com>
2025-06-22 22:28:11 +0800

    update feature store and label store

--- dags/dag.py
+++ dags/dag.py
@@ -16,7 +16,7 @@ with DAG(
     description='data pipeline run once a day',
     schedule_interval='0 9 * * *',  # At 00:00 on day-of-month 1
     start_date=datetime(2016, 9, 4), #2016-09-04 min date
-    end_date=datetime(2018, 9, 3), # end_date=datetime(2017, 12, 3), #'2018-09-03' max date
+    end_date=datetime(2017, 12, 3), # end_date=datetime(2017, 12, 3), #'2018-09-03' max date
     catchup=True,
 ) as dag:
 
@@ -55,33 +55,33 @@ with DAG(
     dep_check_source_label_data >> bronze_label_store >> silver_label_store >> gold_label_store >> label_store_completed
     silver_label_store >> gold_feature_store >> feature_store_completed
 
-    #--- model inference ---
-    model_inference_start = DummyOperator(task_id="model_inference_start")
+#     #--- model inference ---
+#     model_inference_start = DummyOperator(task_id="model_inference_start")
 
-    model_reg_inference = BashOperator(task_id='model_reg_inference',
-        bash_command=(
-            'cd /opt/airflow/scripts &&'
-            'python3 model_inference.py '
-            '--snapshotdate "{{ ds }}" '
-            '--modelname reg_2017_12_04.pkl'
-        ),
-    )
+#     model_reg_inference = BashOperator(task_id='model_reg_inference',
+#         bash_command=(
+#             'cd /opt/airflow/scripts &&'
+#             'python3 model_inference.py '
+#             '--snapshotdate "{{ ds }}" '
+#             '--modelname reg_2017_12_04.pkl'
+#         ),
+#     )
 
-    model_xgb_inference = BashOperator(task_id='model_xgb_inference',
-        bash_command=(
-            'cd /opt/airflow/scripts &&'
-            'python3 model_inference.py '
-            '--snapshotdate "{{ ds }}" '
-            '--modelname xgb_2017_12_04.pkl'
-        ),
-    )
+#     model_xgb_inference = BashOperator(task_id='model_xgb_inference',
+#         bash_command=(
+#             'cd /opt/airflow/scripts &&'
+#             'python3 model_inference.py '
+#             '--snapshotdate "{{ ds }}" '
+#             '--modelname xgb_2017_12_04.pkl'
+#         ),
+#     )
 
-    model_inference_completed = DummyOperator(task_id="model_inference_completed")
+#     model_inference_completed = DummyOperator(task_id="model_inference_completed")
     
-    # Define task dependencies to run scripts sequentially
-    feature_store_completed >> model_inference_start
-    model_inference_start >> model_reg_inference >> model_inference_completed
-    model_inference_start >> model_xgb_inference >> model_inference_completed
+#     # Define task dependencies to run scripts sequentially
+#     feature_store_completed >> model_inference_start
+#     model_inference_start >> model_reg_inference >> model_inference_completed
+#     model_inference_start >> model_xgb_inference >> model_inference_completed
 
 
     # # --- model monitoring ---
